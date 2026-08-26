88c0e58339cb22e7644dde3ad010e8b2a2e98809
cadmium551 <apotatohacker@gmail.com>
2026-08-25 19:34:03 -0700

    Wrote some js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-document.getElementById('gameFileInput')
+document.getElementById('gameFileInput').addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    if (!file) return; //user cancelled el pickle
+
+    if (!isAllowedFile(file)) {
+        alert ('Not .zip or .html, try again');
+        e.target.value = ''; //reset input
+        return;
+    }
+}) 
